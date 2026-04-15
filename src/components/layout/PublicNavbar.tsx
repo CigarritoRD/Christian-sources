46ac6c11f7363558f367b8ab14abf05f36957c0a
Maycolk Hernandez <maycolk.hernandez@gmail.com>
2026-04-15 02:57:20 -0400

    toggle theme

--- src/components/layout/PublicNavbar.tsx
+++ src/components/layout/PublicNavbar.tsx
@@ -126,7 +126,7 @@ export default function PublicNavbar() {
           ) : null}
 
           <LanguageSwitcher />
-          <ThemeToggle />
+          {/* <ThemeToggle /> */}
 
           {loading ? (
             <div className="rounded-2xl border border-surface-border bg-surface px-4 py-2 text-sm text-text-secondary">
